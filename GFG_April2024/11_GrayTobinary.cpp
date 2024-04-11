33c3f61feb46ac8482f7a5ac15c547f833f63733
Sujitmaurya123 <sujitkic6802maurya@gmail.com>
2024-04-11 23:26:28 +0530

    Today 11/04/2024 GFG POTD solution

---
+++ GFG_April2024/11_GrayTobinary.cpp
@@ -0,0 +1,71 @@
+/*
+Gray to Binary equivalent
+
+Question Link ->
+https://www.geeksforgeeks.org/problems/gray-to-binary-equivalent-1587115620/1
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+// User function Template for C++
+
+class Solution
+{
+public:
+    // function to convert a given Gray equivalent n to Binary equivalent.
+    int grayToBinary(int n)
+    {
+
+        // make 1 binary form
+
+        string b = "";
+        for (int i = 0; i <= 31; i++)
+        {
+            if (n & (1 << i))
+            {
+                b = '1' + b;
+            }
+            else
+            {
+                b = '0' + b;
+            }
+        }
+
+        int idx = 0;
+        for (int i = 0; i <= 31; i++)
+        {
+            if (b[i] == '1')
+            {
+                idx = i;
+                break;
+            }
+        }
+        b = b.substr(idx, 32 - idx);
+
+        string g = "";
+        g += b[0];
+        int a = b[0] - '0';
+        for (int i = 1; i < b.length(); i++)
+        {
+            int x = b[i] - '0';
+            if (a ^ x)
+            {
+                g = g + '1';
+                a = 1;
+            }
+            else
+            {
+                g = g + '0';
+                a = 0;
+            }
+        }
+
+        int ans = 0;
+        for (int i = 0; i < g.length(); i++)
+        {
+            ans = ans * 2 + (g[i] - '0');
+        }
+
+        return ans;
+    }
+};
