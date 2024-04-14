cadc945f6249d5bf01c833e22555dca7a8509c27
Sujitmaurya123 <sujitkic6802maurya@gmail.com>
2024-04-14 22:14:38 +0530

    Today 14/04/2024 GFG POTD solution

---
+++ GFG_April2024/14_Xoring.cpp
@@ -0,0 +1,44 @@
+/*
+Xoring and Clearing
+
+Question Link ->
+https://www.geeksforgeeks.org/problems/xoring-and-clearing/1
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// User function Template for C++
+
+class Solution
+{
+public:
+    void printArr(int n, int arr[])
+    {
+        // Your code for printing array here
+        for (int i = 0; i < n; i++)
+        {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+    }
+
+    void setToZero(int n, int arr[])
+    {
+        // Use memset to set arr to zero
+        for (int i = 0; i < n; i++)
+        {
+            arr[i] = 0;
+        }
+    }
+
+    void xor1ToN(int n, int arr[])
+    {
+        // Xor arr[i]^i
+
+        for (int i = 0; i < n; i++)
+        {
+            arr[i] = arr[i] ^ i;
+        }
+    }
+};
